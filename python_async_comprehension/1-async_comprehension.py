9e81c20898586f4edf3b6694e42bea31c5ace330
Stefano-BML <stefanoml1128@gmail.com>
2023-09-01 13:19:46 -0700

    fix: error task

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-""" Async Generator """
+""" Async comprenhesion gen """
 import asyncio
-import random
-from typing import Generator
+from typing import List
 
 
-async def async_generator() -> Generator[float, None, None]:
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
     """
-    Generate numbers
+    Generate numbers with async comprenhension
     """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+    return ([i async for i in async_generator()])
