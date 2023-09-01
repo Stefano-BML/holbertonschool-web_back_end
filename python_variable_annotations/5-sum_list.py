16d8f3aeb90e29426cb1ab85f6791ff965058929
Stefano-BML <stefanoml1128@gmail.com>
2023-09-01 11:13:28 -0700

    List of floats annotations

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+List of floats annotations
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sum of the float numbers
+    """
+
+    result: float = 0
+
+    for x in input_list:
+        result += x
+
+    return result
