af26e4fba5749165080dc758bfacd9de97c151c8
Stefano-BML <stefanoml1128@gmail.com>
2023-10-11 22:58:41 +0000

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,8 +2,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+  return [
+    { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
+    { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason },
+  ];
 }
 
 export default handleProfileSignup;
