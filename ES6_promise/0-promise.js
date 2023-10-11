920a87ae5e02b26f24942c587e9c2fe71d0e997d
Stefano-BML <stefanoml1128@gmail.com>
2023-10-11 22:40:53 +0000

    task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,8 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     // Simulate an API request or any asynchronous operation
     setTimeout(() => {
-      const data = { message: "API response data" };
+      const data = { message: 'API response data' };
       resolve(data);
     }, 2000); // Simulating a 2-second delay
   });
